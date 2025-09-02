66d8a05ec170bed6163508f4bf0a367196ccb4d3
Kavvi <kavita.29oct@gmail.com>
2025-09-03 00:45:25 +0530

    add palindrom problem

--- interview-practice/palindrom-num.js
+++ interview-practice/palindrom-num.js
@@ -18,3 +18,26 @@ function isNumPalidrom(num){
 
 let result = isNumPalidrom(plaindromNum);
 console.log(result);
+
+let num2 = -1234;
+
+function reverseNum(num){
+    let reverseNum = 0;
+    let numCopy = num;
+    num = Math.abs(num); //positive num
+
+    while(num > 0){
+        let getLastDigit = num % 10;
+        reverseNum = (10 * reverseNum) + getLastDigit;
+        //remove last degit
+        num = Math.floor(num/10);
+    }
+    let limit = Math.pow(2, 31);
+    if(reverseNum < -limit || reverseNum > limit) return 0;
+
+    return numCopy < 0 ? -reverseNum : reverseNum;
+}
+
+console.log(reverseNum(num2));
+console.log(reverseNum(9870));
+
